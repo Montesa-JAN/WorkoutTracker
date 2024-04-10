b7d2b33b91fb516362347d549a879904fe9b6750
Montesa-JAN <105424880+Montesa-JAN@users.noreply.github.com>
2024-04-10 15:51:49 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+from datetime import datetime
+import os
+
+WEIGHT_KG = 55
+HEIGHT_CM = 175
+AGE = 22
+GENDER = "Male"
+
+APP_ID = os.environ["APP_ID"]
+API_KEY = os.environ["API_KEY"]
+
+NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+SHEETY_ENDPOINT = os.environ["SHEETY_ENDPOINT"]
+TOKEN = os.environ["TOKEN"]
+
+user_input = input("Tell me which exercises you did: ")
+
+parameters = {
+    "query": user_input,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE,
+}
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+bearer_header = {
+    "Authorization": TOKEN,
+}
+
+response = requests.post(NUTRITIONIX_ENDPOINT, json=parameters, headers=headers)
+data = response.json()
+
+today_now = datetime.now().strftime("%x")
+time_now = datetime.now().strftime("%X")
+
+for exercise in data["exercises"]:
+    exercise_data = {
+        "workout": {
+            "date": today_now,
+            "time": time_now,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+sheety_response = requests.post(SHEETY_ENDPOINT, json=exercise_data, headers=bearer_header)
+
+print(sheety_response)
